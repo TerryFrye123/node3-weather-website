6775511c839de0f503eb201417c6084d0c767d62
TerryFrye123 <terryfrye@verizon.net>
2020-06-24 06:53:18 -0500

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherform.addEventListener('submit', (e) => {
     messageOne.textContent = 'loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location)).then((response) => {
+    fetch('/weather?address=' + encodeURIComponent(location)).then((response) => {
         response.json().then((data) => {
                 if (data.error) {
                     messageOne.textContent = data.error
